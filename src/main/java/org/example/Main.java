038ba42737864b4cfc2c640ab0fbe1ecdb6beaed
vicky <vickykumar29999@gmail.com>
2024-01-05 00:26:17 +0530

    commit 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("vicky");
+        System.out.println(6.87777);
+        String raju="kumar";
+        String x = raju;
+
+        double z=98.99;
+        System.out.println(z);
+        System.out.println("x"+x);
+
+    }
+
+}
